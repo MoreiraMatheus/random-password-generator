c98902bc5320dba8fe79153029b4bc9ce3172989
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-08-20 21:23:06 -0300

    adicionando scripts

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # random-password-generator
  Gerador de senhas aleatório utilizando HTML, CSS e JS
+
+
+## afazeres
+* adicionar a opção de escolher o tamanho da senha
+* adicionar a opção de salvar senha no localstorage
+* adicionar página que mostra as senhas salvas
--- src/JS/index.js
+++ src/JS/index.js
@@ -1,8 +1,43 @@
 const bt_create_random_password = document.querySelector('button')
 const password_panel = document.getElementById('created-password')
 
-let count = 1
-bt_create_random_password.addEventListener('click', () => {
-    password_panel.innerText = `clicaram em mim ${count} vezes`
-    count++
-})
+const checkboxLetters = document.getElementById('iLetter')
+const checkboxCapitalLetters = document.getElementById('iCapitalLetters')
+const checkboxNumbers = document.getElementById('iNumbers')
+const checkboxSpecialCarateres = document.getElementById('iSpecialCaracteres')
+
+let passwordRules = {
+    'letters': false,
+    'captitalizeLetters': false,
+    'numbers': false,
+    'specialCaracteres': false}
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const specialCaracteres = ['!', '/', '@', '#', '$', '%', '&', '*']
+
+function ischecked(parameter){
+    updatePasswordRules(parameter)
+}
+
+function updatePasswordRules(rule){
+    if(rule === 'letters'){
+        passwordRules.letters = !passwordRules.letters
+    }
+    else if(rule === 'captitalizeLetters'){
+        passwordRules.captitalizeLetters = !passwordRules.captitalizeLetters
+
+    }
+    else if(rule === 'numbers'){
+        passwordRules.numbers = !passwordRules.numbers
+    }
+    else{
+        passwordRules.specialCaracteres = !passwordRules.specialCaracteres
+    }
+}
+
+function generatePassword(){
+    console.log(passwordRules)
+}
+
+bt_create_random_password.addEventListener('click', generatePassword)
