66147135534be10b9b658c455e9ecb22930e0a80
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-08-23 22:32:35 -0300

    adicionando funcionalidades para a segunda tela

--- src/JS/savedPassword.js
+++ src/JS/savedPassword.js
@@ -1,2 +1,16 @@
 const main = document.querySelector('main')
 
+for(let i = 0; i < localStorage.length; i++){
+    main.innerHTML += 
+    `<div class="password-saved">
+        <span>${localStorage.getItem(localStorage.key(i))}</span>
+        <button onclick="deletePassword('${localStorage.key(i)}')" class="icon-button">
+            <img src="../img/trash.png" alt="trash">
+        </button>
+    </div>`
+}
+
+function deletePassword(key){
+    // window.alert(key)
+    localStorage.removeItem(key)
+}
